d5e7b76ab3df380842acbc33b4a5b57801cf393a
Yannick Uhlmann <augustunderground@protonmail.com>
2024-12-03 14:33:17 +0100

    day 03 washed

--- src/Day03.pl
+++ src/Day03.pl
@@ -3,22 +3,20 @@
 use strict;
 use warnings;
 
-my $file = './rsc/day03.txt';
-open my $info, $file or die "Could not open $file: $!";
+my $file    = "./rsc/day03.txt";
+my $program = "";
+open my $content, $file or die "Could not open $file: $!";
+while(my $line = <$content>) { chomp($line); $program .= $line; }
+close $content;
 
 my $regex1 = qr/mul\((\d+),(\d+)\)/;
+my $total1 = 0;
 my $regex2 = qr/don't\(\).*?do\(\)|don't\(\).*$/;
+my $total2 = 0;
 
-my $total1  = 0;
-my $total2  = 0;
-my $program = "";
-
-while(my $line = <$info>) { chomp($line); $program .= $line; }
 while ($program =~ /$regex1/g) { $total1 = $total1 + ($1 * $2); }
 $program =~ s/$regex2//g;
 while ($program =~ /$regex1/g) { $total2 = $total2 + ($1 * $2); }
 
 print "Silver: ${total1}\n";
 print "Gold:   ${total2}\n";
-
-close $info;
